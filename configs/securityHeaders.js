eb71d242fd9641f8aeb3ad1ca673c05b51d13a4f
tpatalas <tp@atalas.dev>
2023-09-19 17:45:04 -0500

    chore: Update the basic setup
    Update tsconfig, tailwind.configs, next.config, and lintstagedrc.

---
+++ configs/securityHeaders.js
@@ -0,0 +1,59 @@
+const scriptSources = require("./scriptSources");
+
+const development = process.env.NODE_ENV !== "production";
+
+const ContentSecurityPolicy = `
+  default-src 'self'; 
+  connect-src 'self' ${process.env.NEXT_PUBLIC_IMAGE_DOMAIN};  
+  child-src 'self' youtube.com;
+  font-src 'self';
+  img-src 'self' ${process.env.NEXT_PUBLIC_IMAGE_DOMAIN} data:;
+  style-src 'self' 'unsafe-inline';
+  ${
+    development
+      ? `script-src 'self' 'unsafe-eval' 'unsafe-inline' ${scriptSources}`
+      : `script-src 'self' ${scriptSources}`
+  };
+  ${development ? "" : `upgrade-insecure-requests`}
+  `;
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=(),  payment=()",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+  {
+    key: "Access-Control-Allow-Origin",
+    value: process.env.NEXT_PUBLIC_HOST,
+  },
+];
+
+module.exports = securityHeaders;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const securityHeaders = require("./configs/securityHeaders");
+const imageDomains = require("./configs/imageDomains");
 
-module.exports = nextConfig
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+  openAnalyzer: true,
+  //Run for analyze build: ANALYZE=true yarn build
+});
+
+module.exports = withBundleAnalyzer({
+  poweredByHeader: false,
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  images: {
+    // loader: 'custom',
+    // loaderFile: './app/_lib/utils/imageLoader.utils.ts',
+    // domains: imageDomains,
+  },
+  output: "standalone",
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "production" ? false : true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
+});
